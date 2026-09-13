e7db934054182f74be0f842691a792025eebd1e4
quyet <quyetbkhoa@gmail.com>
2026-09-14 03:47:58 +0700

    chore: release v1.2.0 with catalog and download fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kiwi.manager"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10000
-        versionName = "1.0.0"
+        versionCode = 10200
+        versionName = "1.2.0"
         
         vectorDrawables {
             useSupportLibrary = true
