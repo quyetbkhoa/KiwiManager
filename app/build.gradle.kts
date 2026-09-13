b1501ebff88e5c7b5f951392a94fce5c8e187ffa
quyet <quyetbkhoa@gmail.com>
2026-09-14 04:04:33 +0700

    chore: bump version to v1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kiwi.manager"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10200
-        versionName = "1.2.0"
+        versionCode = 10201
+        versionName = "1.2.1"
         
         vectorDrawables {
             useSupportLibrary = true
